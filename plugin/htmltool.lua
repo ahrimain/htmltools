62522c84afeab4f9aa3d58c9a7040cfe76e8f8bb
Tobias Hauge <tobiashauge1@gmail.com>
2025-01-15 19:55:58 +0100

    htmlsetup command

--- plugin/htmltool.lua
+++ plugin/htmltool.lua
@@ -1,5 +1,15 @@
-local setup = function ()
-    
-end
+vim.api.nvim_create_user_command('HtmlSetup', function ()
+    vim.api.nvim_buf_set_lines(0, 0, 9, false, {
+        '<html lang="en">',
+        '<head>',
+        '    <meta charset="UTF-8">',
+        '    <meta name="viewport" content="width=device-width, initial-scale=1.0">',
+        '    <title>Document</title>',
+        '</head>',
+        '<body>',
+        '',
+        '</body>',
+        '</html>'
 
-vim.api.nvim_create_user_command("htmltools", setup(), {})
+    })
+end, {})
