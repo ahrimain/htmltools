e74dce6c1a4f04c528448d9afced5efc7a0f254d
Tobias Hauge <tobiashauge1@gmail.com>
2025-01-15 20:11:16 +0100

    split html setup

--- plugin/htmltool.lua
+++ plugin/htmltool.lua
@@ -1,15 +1,24 @@
-vim.api.nvim_create_user_command('HtmlSetup', function ()
-    vim.api.nvim_buf_set_lines(0, 0, 9, false, {
-        '<html lang="en">',
-        '<head>',
-        '    <meta charset="UTF-8">',
-        '    <meta name="viewport" content="width=device-width, initial-scale=1.0">',
-        '    <title>Document</title>',
-        '</head>',
-        '<body>',
-        '',
-        '</body>',
-        '</html>'
+local htmlsetup = {
+    '<html lang="en">',
+    '<head>',
+    '    <meta charset="UTF-8">',
+    '    <meta name="viewport" content="width=device-width, initial-scale=1.0">',
+    '    <title>Document</title>',
+    '</head>',
+    '<body>',
+    '',
+    '</body>',
+    '</html>'
 
-    })
+}
+
+vim.api.nvim_create_user_command('HtmlSetup', function ()
+    vim.api.nvim_buf_set_lines(0, 0, 9, false, htmlsetup)
+end, {})
+
+vim.api.nvim_create_user_command('HtmlAssets', function ()
+    vim.fm.mkdir('assets', '-p')
+    vim.fm.writefile({
+        '/*l ink css to html with <link> tag */'
+    }, 'assets/main.css')
 end, {})
